e675f7834b8c1a8af473770e5dc136b27c41d29b
Stefan H <hutanus@tcd.ie>
2021-03-19 00:04:37 +0000

    Back end reference (#14)
    * Created a basic express app
    
    * Added the test route for /multi_store_customers

---
+++ back-end-reference/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Visit http://localhost:3000/multi_store_customers?store=001 in your browser to test this
+app.get('/multi_store_customers', (req, res) => {
+    if(req.query.store === undefined)
+        res.sendStatus(400)
+    else if(req.query.store === "001")
+        res.send({
+            "multi_store_customers": 10,    // Customers that visited this store, as well as another store
+            "single_store_customers":  20   // Customers that only visited this store
+        })
+    else
+        res.sendStatus(400)
+})
+
+app.listen(port, () => {
+    console.log(`Back end api reference listening at http://localhost:${port}`)
+    console.log(`Restart node.js after making changes`)
+})
