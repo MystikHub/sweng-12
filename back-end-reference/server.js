49f1b8d52c05019b8adda3cff1789b2ed489e86c
JamesFenzy09 <fenlon123@gmail.com>
2021-03-24 23:39:25 +0000

    read each of the keys and full json

--- back-end-reference/server.js
+++ back-end-reference/server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+//var url = require('url'); //Split up the web address
+const fs = require("fs"); //File system
 
 app.get('/', (req, res) => {
-  res.send(`Hi! Server is listening on port ${port}`)
+    let findJson = fs.readFileSync('./totals.json');
+    let wholeJson = JSON.parse(findJson);
+    var allObjects = Object.keys(wholeJson)
+    var obj=[] // all asked stores
+    allObjects.forEach(value => {
+            if (value.includes("PAV")) {
+                obj.push(value);
+            }
+        })
+    res.send(obj)
 });
 
+/*
+let findJson = fs.readFileSync('./totals.json');
+    let wholeJson = JSON.parse(findJson);
+    let finger = res.json(wholeJson[PAV])
+    res.send(finger)
+    */
+
+
 // listen on the port
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Back end api reference listening at http://localhost:${port}`)
+    console.log(`Restart node.js after making changes`)
+})
+
