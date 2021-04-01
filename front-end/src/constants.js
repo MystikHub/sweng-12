82a0d484c7477473eeebc6cb2ffc7170c8fd4da0
Mystik <stefan.address@gmail.com>
2021-04-01 20:42:59 +0100

    Made charts slightly smaller

--- front-end/src/constants.js
+++ front-end/src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
     api_server: 'http://localhost:3000',
-    chart_width: 550,
-    chart_height: 550
+    chart_width: 500,
+    chart_height: 500
 }
 
 module.exports = constants;
