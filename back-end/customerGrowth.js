7ac6eec0bab3c599195250084beabcfa32d0b358
AvalonXD <ImoesiIdogho@yahoo.ie>
2021-04-01 14:33:41 +0100

    Fixed the undefined error

--- back-end/customerGrowth.js
+++ back-end/customerGrowth.js
@@ -1,22 +1,25 @@
 const fs = require("fs"); //File system
 
-async function customerGrowth (req, res) {
+async function customerGrowth(req, res) {
 
-    fs.readFile ("./backendData/totals.json", function(err, data) { 
+    fs.readFile("./backendData/totals.json", function (err, data) {
         const obj = JSON.parse(data)
         var customer_growth_over_time = []
         var timescale_of_the_dates = []
 
         for (let locations in obj) {
-            const timeStamps = Object.keys(obj[locations]["Month Unique Users"])
-            
-            timeStamps.forEach(date => {
-                timescale_of_the_dates.push (date)
-            })
+            if (obj[locations]["Month Unique Users"]) {
+                const timeStamps = Object.keys(obj[locations]["Month Unique Users"])
+                const userNumbers = Object.values(obj[locations]["Month Unique Users"])
+                console.log(obj[locations]["Month Unique Users"])
 
-            timeStamps.forEach(value => {
-                customer_growth_over_time.push (value)
-            })
+                timeStamps.forEach(date => {
+                    timescale_of_the_dates.push(date)
+                })
+                userNumbers.forEach(users => {
+                    customer_growth_over_time.push(users)
+                })
+            }
         }
         res.send({
             "customer_growth_over_time": customer_growth_over_time,
