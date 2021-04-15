e07ade893ebda86ba28c1eecc3f4c3184e358905
JamesFenzy09 <fenlon123@gmail.com>
2021-04-15 12:38:32 +0100

    fixed voucher size

--- back-end/mostPopularVoucherSize.js
+++ back-end/mostPopularVoucherSize.js
@@ -1,38 +1,36 @@
 const fs = require("fs"); //File system
 
 let unused = [];
-let stampss=[]
+let packageSize = [];
 
-function totalStamps(obj){
-    let inner_obj = {};    
+function totalStamps(obj) {
+  let inner_obj = {};
 
-    for(prop in obj){
-        if(typeof(obj[prop])=="object"){
-            totalStamps(obj[prop]);
-        }
-        else{
-            if(prop=="Stamps"){
-                inner_obj[prop] = obj[prop];
-                stampss.push(inner_obj);
-            }
-        }
+  for (prop in obj) {
+    if (typeof obj[prop] == "object") {
+      totalStamps(obj[prop]);
+    } else {
+      if (prop == "Amount") {
+        inner_obj[prop] = obj[prop];
+        packageSize.push(Object.values(inner_obj));
+      }
     }
+  }
 }
 
-function totalUnredeemedStamps(obj){
-    let inner_obj = {};    
+function totalUnredeemedStamps(obj) {
+  let inner_obj = {};
 
-    for(prop in obj){
-        if(typeof(obj[prop])=="object"){
-            totalUnredeemedStamps(obj[prop]);
-        }
-        else{
-            if(prop=="Unredeemed Vouchers"){
-                inner_obj[prop] = obj[prop];
-                unused.push(inner_obj);
-            }
-        }
+  for (prop in obj) {
+    if (typeof obj[prop] == "object") {
+      totalUnredeemedStamps(obj[prop]);
+    } else {
+      if (prop == "Unredeemed Vouchers") {
+        inner_obj[prop] = obj[prop];
+        unused.push(inner_obj);
+      }
     }
+  }
 }
 
 async function mostPopularVoucherSize(req, res) {
@@ -41,42 +39,73 @@ async function mostPopularVoucherSize(req, res) {
   } else {
     let findTotalsJson = fs.readFileSync("./backendData/totals.json");
     let totalsJson = JSON.parse(findTotalsJson);
-    let findRawRedemptionsJson = fs.readFileSync("./backendData/rawredemptions.json");
+    let findRawRedemptionsJson = fs.readFileSync(
+      "./backendData/rawredemptions.json"
+    );
     let rawredemptionsJson = JSON.parse(findRawRedemptionsJson);
+
+    let findVouchersJson = fs.readFileSync("./backendData/vouchers.json");
+    let vouchersJson = JSON.parse(findVouchersJson);
 
     var allSeries = Object.keys(totalsJson);
     var allLocations = Object.keys(rawredemptionsJson);
     var allStamps = Object.entries(totalsJson);
-    var unredeemed=[]
+    var allVouchers = Object.entries(vouchersJson);
+    var unredeemed = [];
     var locations = []; // all locations of company
     var stampy = []; //all stamps
+    var packageTypes = [];
+
+    var tally=0
+    let tallyCount=[]
+    var duplicate=true;
 
     allLocations.forEach((value) => {
       if (value.includes("-")) {
         locations.push(value);
       }
     });
-    allStamps.filter((item) => {
-        totalStamps(item);
+    allVouchers.filter((item) => {
+      totalStamps(item);
     });
-    allStamps.filter(item=>{
-        totalUnredeemedStamps(item)
-    })
-    for (let i = 0; i < stampss.length; i++) {
-      stampy[i] = Object.values(stampss[i]); //last one in the array will be the total number between every store and every scheme
+    allStamps.filter((item) => {
+      totalUnredeemedStamps(item);
+    });
+    packageSize = packageSize.flat()
+    for (var i=0; i < packageSize.length; i++) {
+      if(packageTypes.includes(packageSize[i])){
+        
+      }
+      else{
+        packageTypes.push(packageSize[i])
+        console.log("passing through")
+        for (var j=0; j < packageSize.length; j++) {
+          if (packageSize[i] === packageSize[j] ) {
+            tally = tally + 1
+          }
+        }
+        tallyCount.push(tally)
+        tally=0
+      }
+        
     }
-    unredeemed[0] = Object.values(unused[0])
+    packageSize = [];
+    unredeemed[0] = Object.values(unused[0]);
 
     res.send({
-      label1: allLocations[0],
-      label2: allLocations[1],
-      label3: allLocations[2],
-      label4: "Unredeemed",
+      label1: "Package Size: " + packageTypes[0],
+      label2: "Package Size: " + packageTypes[1],
+      label3: "Package Size: " + packageTypes[2],
+      //label4: "Unredeemed",
 
-      value1: stampy[0],
-      value2: stampy[1],
-      value3: stampy[2],
-      value4: unredeemed,
+      value1: tallyCount[0],
+      value2: tallyCount[1],
+      value3: tallyCount[2],
+      //value4: unredeemed,
+      "package":packageSize,
+      packageTypes,
+      tallyCount,
+      "length":packageSize.length
     });
   }
 }
